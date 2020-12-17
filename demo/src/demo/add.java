db5bc116119ee2648f39fc5e46568dfe24f382d3
sangeeethragi <sangeeth.ragi@gmail.com>
2020-12-17 17:23:22 +0530

    new branch

---
+++ demo/src/demo/add.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class add {
+	
+	public void add(int n1,int n2) {
+		int m=n1+n2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

a5191c8e9990b49b0a8420fa414363dea6504875
sangeethragi <sangeeth.ragi@gmail.com>
2020-12-17 13:51:00 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,16 @@ public static int add(int n1,int n2){
   return (n1+n2)
 
 }
+  public static int diff(int n1,int n2){
+
+  return (n1-n2)
+
+}
 
 public static void main(String args[]){
 
 System.out.println(add(12,10));
+  System.out.println(diff(12,10));
 }
 
 

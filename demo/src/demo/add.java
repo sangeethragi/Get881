59a82d9d8bcc38b1c62a83de5c94144dce2c5e4a
sange <sange@192.168.0.80>
2020-12-17 17:44:36 +0530

    subs

--- demo/src/demo/add.java
+++ demo/src/demo/add.java
@@ -1,9 +1,12 @@
 package demo;
 
 public class add {
+	public void subs(int n1,int n2) {
+		int m=n1-n2;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
